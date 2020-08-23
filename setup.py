19c52829e066adca81c06233391b6144c6d60d2c
Daniel Porter <dpreid@gmail.com>
2020-08-23 17:06:35 +0100

    Linting and docstring changes
    Many linting improvements were made, and docstrings added where missing.
    The requests library generates members within requests.codes, so
    definitions for this were added for pylint.

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+
 from distutils import log
+from setuptools import find_packages
+from setuptools import setup
 
 try:
     import pypandoc
@@ -8,17 +11,15 @@ except ImportError:
     log.warn("warning: Couldn't generate README.rst - is pypandoc installed?")
 
 setup(
-    name = "doxieapi",
-    version = "0.0.2",
-    packages = find_packages(),
-
-    install_requires = ['requests'],
-
-    author = "Dave Arter",
-    author_email = "pypi@davea.me",
-    description = "Library for downloading scans from a Doxie Go Wi-Fi document scanner",
-    license = "LICENSE.txt",
-    keywords = "doxie document scanner",
-    url = "https://github.com/davea/doxieapi/",
-    python_requires = ">=3.4",
+    name="doxieapi",
+    version="0.0.2",
+    packages=find_packages(),
+    install_requires=['requests'],
+    author="Dave Arter",
+    author_email="pypi@davea.me",
+    description="Library for downloading scans from a Doxie Go Wi-Fi document scanner",
+    license="LICENSE.txt",
+    keywords="doxie document scanner",
+    url="https://github.com/davea/doxieapi/",
+    python_requires=">=3.4",
 )
