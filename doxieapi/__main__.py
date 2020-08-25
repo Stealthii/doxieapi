c1b7ed9d952908ec9498726507ea12aca6106ac0
Daniel Porter <dpreid@gmail.com>
2020-08-26 00:44:59 +0100

    Add argument features to main method
    Allow definition of servers, defaulting to auto-discovery if undefined.
    Add option of deleting scans after successful download.
    Add configurable output directory, defaulting to the current directory.

--- doxieapi/__main__.py
+++ doxieapi/__main__.py
@@ -7,21 +7,68 @@ doxieapi.__main__
 Application that runs when executing the module with -m.
 """
 
+import argparse
 import os
+import sys
 
 from .api import DoxieScanner
 
 
-def main():
+def main(servers=None, delete=False, output_dir=None):
     """
     Grab all available scan images and save them to the current working
     directory.
     """
-    for doxie in DoxieScanner.discover():
-        print("Discovered {}.".format(doxie))
-        for scan in doxie.download_scans(os.getcwd()):
+
+    if servers:
+        print("{} scanners defined, connecting...".format(len(servers)))
+        doxies = [DoxieScanner(server) for server in servers]
+    else:
+        print("No scanners defined, trying auto-discover...")
+        doxies = DoxieScanner.discover()
+    if not doxies:
+        print("No scanners discovered, exiting")
+        sys.exit(1)
+    for doxie in doxies:
+        print("Connecting to {}.".format(doxie))
+        names = [scan['name'] for scan in doxie.scans]
+        if not names:
+            print("No scans found, skipping...")
+            continue
+        print("Downloading {} scans...".format(len(names)))
+        saves = [
+            os.path.basename(name)
+            for name in doxie.download_scans(names, output_dir)
+        ]
+        for scan in saves:
             print("Saved {}".format(scan))
+        print("Successfully downloaded {} of {} scans".format(len(saves),
+                                                              len(names)))
+        if delete:
+            # Only delete files we have successfully downloaded
+            names_to_delete = [
+                name for name in names
+                if name.endswith(tuple(saves))
+            ]
+            print("Deleting {} successfully downloaded scans...".format(
+                len(names_to_delete)))
+            if doxie.delete_scans(names_to_delete):
+                print("Successfully deleted scans!")
+            else:
+                print("Failed to delete scans.")
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        description="Doxie scanner auto-download tool")
+    parser.add_argument(
+        '--server', dest='servers', action='append',
+        help="Doxie server to connect to. Multiple possible.")
+    parser.add_argument(
+        '--delete', action='store_true',
+        help="Automatically remove scans after successful download")
+    parser.add_argument(
+        '--output-dir', default=os.getcwd(),
+        help="Output directory for the scans")
+    args = parser.parse_args()
+    main(**vars(args))
